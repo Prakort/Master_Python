6ee793bbfb900838e6823092d0821657a433d4f6
Prakort <leanprakort@gmail.com>
2020-06-02 10:43:25 -0400

    map api

---
+++ map.py
@@ -0,0 +1,27 @@
+# Map
+# class dict(**kwarg)
+# class dict(mapping, **kwarg)
+# class dict(iterable, **kwarg)
+
+a = dict(one = 1, two = 2, three = 3)
+b = {'one' : 1, 'two' : 2, 'three' : 3}
+c = dict(zip(['one', 'two', 'three'], [1,2,3]))
+d = dict([('one',1),('two',2),('three',3)])
+e = dict({'three': 3, 'one': 1, 'two': 2})
+
+print( a == b == c == d == e)
+
+# list(d) return a list of all keys
+li = list(a)
+print('list(d)',list(li),'len(d)', len(li))
+
+el = d['one']
+print(el)
+b = a
+del a['one']
+print('shallow copy')
+print('del a["one"]',a , 'b := a ',b)
+
+# key not in dict 
+# key is not in dict is wrong
+print( 'two' not in a)
