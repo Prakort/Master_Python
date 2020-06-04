e489a5eed90b632a0e62a16a41e1c9ae03ac43ae
Prakort <leanprakort@gmail.com>
2020-06-04 11:03:43 -0400

    invert binary tree recursive and iterative

--- invert-binary-tree.py
+++ invert-binary-tree.py
@@ -2,9 +2,18 @@ class Solution:
   def invertTree(self, root: TreeNode) -> TreeNode:
     
     return self.recursive(root)
+
+
   # pass parent node in arg
   # swap the children the parent node
   # apply recursive to the child node as the parent of their own nodes
+
+
+  # Complexity: each node is the tree is visited only once
+  # time complexity is O(n) where n is the number of the nodes in the tree
+  # This is recursive function, the function will be places on the stack
+  # in the worst case, where H is the height of the tree then
+  # space complexity is O(H)
   def recursive(self,root):
     if root is None:
       return 
@@ -15,4 +24,17 @@ class Solution:
     return root
       
     
-     
+  # time and space complexity are O(n) 
+  # everyone node is visited and we use a stack to store all the nodes to swap their children
+  def iterative(self,root):
+    stack = [root]
+    while stack:
+      node = stack.pop()
+      if node is not None:
+        node.right, node.left = node.left, node.right
+        if node.right:
+          stack.append(node.right)
+        if node.left:
+          stack.append(node.left)
+        
+    return root
