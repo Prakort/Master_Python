d6c67ce5823151c974f4291a5003ca0cc1293987
Prakort <leanprakort@gmail.com>
2020-06-23 12:36:44 -0400

    iterative height of tree

--- longest-valid-parentheses.py
+++ longest-valid-parentheses.py
@@ -7,17 +7,18 @@ def longestValidParentheses(self, s: str) -> int:
   find the lenght index - top index which could be -1, to get the offset
   or if the stack is empty because of the first close ')' add the current index to restore the index
   """
-  len_s = len(s)
-  stack, max_len = [], 0
+  stack = []
+  max_len = 0
   stack.append(-1)
-  for i in range(len_s):
+
+  for i in range(len(s)):
     if s[i] == '(':
       stack.append(i)
-    else:
+    else 
       stack.pop()
       if not stack:
         stack.append(i)
       else:
-        max_len = max(max_len, i - stack[-1])
-        print('i',i,'top',stack[-1])
+        max_len = max(max_len , i - stack[-1])
+
   return max_len
