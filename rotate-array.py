986a217ceacac0b84a5dcb1a888c61816febab95
Prakort <leanprakort@gmail.com>
2020-06-25 23:34:45 -0400

    rotate k steps

---
+++ rotate-array.py
@@ -0,0 +1,23 @@
+def solution(nums, k):
+  """
+  rotate array with k steps
+  it is the same, reverse the whole, reverse [0..k] and reverse [k..n]
+  """
+  n = len(nums)
+  k %= n 
+
+  # reverse the whole array
+  reverse(nums,0,n-1,n)
+  # reverse the first k
+  reverse(nums,0,k-1)
+  # reverse the the rest of array
+  reverse(nums,k,n-1)
+
+def reverse(nums, l , r, n):
+  while l < r < n:
+    nums[l], nums[r] = nums[r] , nums[l]
+    l += 1
+    r -=1
+
+
+
