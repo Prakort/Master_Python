c1c6cae42827bead5dbe9597f23480896c9a944b
Prakort <leanprakort@gmail.com>
2020-06-27 02:33:55 -0400

    convert int to hex

--- convert-a-number-to-hexadecimal.py
+++ convert-a-number-to-hexadecimal.py
@@ -1,5 +1,13 @@
 def solution(num):
-
+  
+  # base 16 
+  # remainder = nums % 16
+  # nums = (nums - remainder) / 16
+  # if nums < 0
+  # follow algo -> find the binary value of -nums ==> find the complement of binary value and add + 1 is the second complement
+  # second complement in binary then convert it to hex
+  # 2 ** 32 + nums = (new value) which fits the second complement of nums
+  
 
   if num == 0:
     return '0'
@@ -13,5 +21,5 @@ def solution(num):
     num -= r
     num //= 16
     res.append(str(r) if r <= 9 else l[r-10])
-    
+
   return ''.join(res[::-1])
