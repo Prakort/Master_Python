0bca85368f09098d590a851a33aae12a7bb886c1
Prakort <leanprakort@gmail.com>
2020-06-27 20:38:10 -0400

    update comment for subarray continuous sum equals K

--- subarray-sum-equals-k.py
+++ subarray-sum-equals-k.py
@@ -3,6 +3,18 @@ def subarraySum(self, nums: List[int], k: int) -> int:
   Input:nums = [1,1,1], k = 2
   Output: 2
   
+  Idea: sum([i,j]) = k this is substring? 
+  Fact: how to find sum([i,j])? we know that sum[i] = sum([0,i]) and sum[j] = sum(0,j)
+  ==> sum([i,j]) = sum[0,j] - sum[0,i] 
+  ==> sum[0,j] - sum[0,i] = K we could store the sum[0,i] in the hashmap to look up as the compliment
+  ==> sum[0,j] - K = sum[0,i] if sum[0,i] exists in the compliment that means we find the SUBSTRING sum == K
+
+  we keep update sum[0,j] since that is the cumulative sum as the current running sum up to j index
+
+  if the sum[0,j] already exits we keep increment count that mean, there more than one possible continunous substring sum that equal to K
+
+
+
   map: [
   (0:1),
   (1:1), 
