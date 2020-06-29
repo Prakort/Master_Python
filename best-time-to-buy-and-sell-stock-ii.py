e39574090bc0fdd88ffa52e06e5ba93b1eaa5614
Prakort <leanprakort@gmail.com>
2020-06-28 23:56:56 -0400

    buy and sell stock ii

--- best-time-to-buy-and-sell-stock-ii.py
+++ best-time-to-buy-and-sell-stock-ii.py
@@ -9,3 +9,32 @@ def solution(prices):
       maxProfit += prices[i] - prices[i-1]
 
   return maxProfit
+
+def valley_peek(prices):
+  """
+  Same idea as mention, we want to make max profit with many transactions
+  then we first must find the LOCAL minimum and its next LOCAL maximum and make transaction,
+  and keep repeating the process until no more stock
+  """
+  n = len(prices)
+  maxProfit = i = 0 
+  localMin = localMax = prices[0]
+  
+  while i < n - 1:
+    # keep iterating to find the LOCAL min
+    while i < n and prices[i] >= prices[i+1]:
+      i +=1
+    # while loop break that means we find the local min
+    localMin = prices[i]
+
+    # keep iterating to find the LOCAL max
+    while i < n and prices[i] <= prices[i-1]:
+      i +=1
+    # we find the local max
+    localMax = prices[i]
+
+    maxProfit += localMax - localMin
+  
+  return maxProfit
+
+
