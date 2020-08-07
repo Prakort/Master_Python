d137d383350fc756185aa2d6dc7b12b101d6639a
Prakort <leanprakort@gmail.com>
2020-08-07 13:56:24 -0400

    bad orange

--- angle-between-hands-of-a-clock.py
+++ angle-between-hands-of-a-clock.py
@@ -3,8 +3,8 @@ def solution(hour, minutes):
   # 1 h represents 30 degree 
   # 60 minutes on the cloco
   # 1 m represents 6 degree
-
-  h = (hour + minutes/60) * 30 # convert minute to hour, hour hand points to the minutes between hour
+  # cases hour is 24h or 12 ?
+  h = (hour%12 + minutes/60) * 30 # convert minute to hour, hour hand points to the minutes between hour
   m = minutes * 6
 
   # find the angle of the hour and minute
